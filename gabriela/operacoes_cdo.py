e94287775b1886f7fe437ed84587c99d5a8664d1
gabrielavitelli <gabrielavitelli@gmail.com>
2024-04-29 09:32:04 -0300

    recorte para efc

--- gabriela/operacoes_cdo.py
+++ gabriela/operacoes_cdo.py
@@ -7,6 +7,10 @@ import cartopy.io.shapereader as shpreader
 import cartopy.feature as cfeature
 import geopandas as gpd
 import pandas as pd
+import rioxarray
+from shapely.geometry import mapping
+
+#####################################################################################################################################
 #/mnt/c/Users/Usuario/gabriela/
 entrada='/mnt/c/Users/Usuario/gabriela//dados/CORDEX/pr_SAM-20_MIROC-MIROC5_rcp85_r1i1p1_INPE-Eta_v1_day_20960101-20991231.nc'
 
@@ -21,58 +25,10 @@ print (dado_bruto)
 #plt.show()
 #quit()
 
-Min_Longitude = -50.12517419989768
-Min_Latitude = -6.014153889871978
-Max_Longitude = -44.29318420965952
-Max_Latitude = -2.621573681489544
 saida='/mnt/c/Users/Usuario/gabriela'
-cdo = Cdo()
-
-print(cdo.version())
-
-tempPath = './tmp/'
-cdo = Cdo(tempdir=tempPath)
 
 
-cdo.cleanTempDir()
-
-#print (cdo.operators)
-cdo.debug=True
-#print(cdo.sinfon(input=entrada))
-
-
-#ds = xr.open_dataset(entrada)
-#print(ds.info())
-
-cdo.copy(input=entrada, options='-b F64')
-#cdo.sinfon(input='outfile.nc')
-
-#muda de kg/m2/s para mm/dia
-cdo.mulc(86400, input=entrada, output='mmday.nc')
-
-cdo.selvar('pr', input='mmday.nc', returnXArray='pr', output=f'{saida}/pr.nc')
-cdo.seltimestep('1/30', input=f'{saida}/pr.nc', options='-b F64', output=f'{saida}/janeiro.nc')
-
-cdo.sellonlatbox(Min_Longitude,Max_Longitude,Min_Latitude,Max_Latitude, input=f'{saida}/janeiro.nc', output=f'{saida}/area.nc')
-
-#cdo.remapbil ('r648x324', input=f'{saida}/area.nc', output=f'{saida}/interpolacao.nc')
-#cdo.remapbic ('r720x360', input=f'{saida}/area.nc', output=f'{saida}/interpolacao.nc')
-#data = xr.open_dataset(f'{saida}/area.nc')
-
-#ds_interp = data.interp(method='linear')
-#cdo.remapbil ('r360x180', input=f'{saida}/area.nc', output=f'{saida}/interpolacao.nc')
-# 360 180
-
-data = xr.open_dataset(f'{saida}/area.nc')
-
-#print (data)
-#fig, ax = plt.subplots(figsize=(10, 6), subplot_kw={'projection': ccrs.PlateCarree()})
-#data['pr'].isel(time=0).plot(ax=ax, transform=ccrs.PlateCarree())
-#plt.title('Sem interpolacao')
-#plt.show()
-
-
-
+#####################################################################################################################################
 # shapefiles #
 caminho_geoinfra = "/mnt/c/Users/Usuario/gabriela/dados/shp/"
 shp_caminho_pa = f'{caminho_geoinfra}PA_Municipios_2022/PA_Municipios_2022.shp'
@@ -80,43 +36,81 @@ shp_pa= gpd.read_file(shp_caminho_pa)
 shp_caminho_ma = f'{caminho_geoinfra}MA_Municipios_2022/MA_Municipios_2022.shp'
 shp_ma=gpd.read_file(shp_caminho_ma)
 # efc #
-shp= gpd.read_file("/mnt/c//Users/Usuario/gabriela/dados/shp/EFC/extensao_efc_shp.shp")
+shp= gpd.read_file("/mnt/c/Users/Usuario/gabriela/dados/shp/EFC/extensao_efc_shp.shp", crs="epsg:4326")
 
 
+#####################################################################################################################################
+cdo = Cdo()
+print(cdo.version())
+
+tempPath = './tmp/'
+cdo = Cdo(tempdir=tempPath)
+cdo.cleanTempDir()
+
+#print (cdo.operators)
+cdo.debug=True
+#print(cdo.sinfon(input=entrada))
+#ds = xr.open_dataset(entrada)
+#print(ds.info())
+
+cdo.copy(input=entrada, options='-b F64')
+#cdo.sinfon(input='outfile.nc')
+#muda de kg/m2/s para mm/dia
+cdo.mulc(86400, input=entrada, output='mmday.nc')
+cdo.selvar('pr', input='mmday.nc', returnXArray='pr', output=f'{saida}/pr.nc')
+cdo.seltimestep('1/30', input=f'{saida}/pr.nc', options='-b F64', output=f'{saida}/janeiro.nc')
+bbox = shp.total_bounds
+min_lon, min_lat, max_lon, max_lat = bbox
+cdo.sellonlatbox(min_lon, max_lon, min_lat, max_lat, input=f'{saida}/janeiro.nc', output=f'{saida}/area.nc')
+
+
+data = xr.open_dataset(f'{saida}/area.nc')
+
+#####################################################################################################################################
+# mascara #
+data = data.rio.set_spatial_dims(x_dim="lon", y_dim="lat", inplace=True)
+
+#  buffer em torno da linha 
+buffer = shp.buffer(0.5)
+# Converter o xarray dataset para um rioxarray para facilitar a operação de corte
+data_rio = data.rio.write_crs("EPSG:4326")  
+# Recortar o dataset usando o buffer
+clipped_ds = data_rio['pr'].rio.clip(buffer.geometry, buffer.crs)
+# Salvar o resultado ou fazer mais operações
+clipped_ds.to_netcdf("saida_clipped.nc")
+recortado= xr.open_dataset(f'{saida}/saida_clipped.nc')
+#####################################################################################################################################
 
 # plotagem #
 fig, ax = plt.subplots(figsize=(10, 6), subplot_kw={'projection': ccrs.PlateCarree()})
 ax.set_xlim([shp.bounds.minx.min(), shp.bounds.maxx.max()])
 ax.set_ylim([shp.bounds.miny.min(), shp.bounds.maxy.max()])
 
-#plt.contourf(ds_interp['lon'], ds_interp['lat'], ds_interp['pr'], cmap='viridis')
-cbar_kwargs = {
-    'label': 'Precipitação (mm/dia)',  # Título da colorbar
-    'orientation': 'vertical'
-}
-data['pr'].isel(time=0).plot(ax=ax, transform=ccrs.PlateCarree(), cbar_kwargs={'label':'precipitação (mm/dia'})
-#colorbar = fig.colorbar(pr, ax=ax, orientation='vertical')
-#colorbar.set_label('Precipitação (mm/dia)') 
+recortado['pr'].isel(time=0).plot(ax=ax, transform=ccrs.PlateCarree(), cbar_kwargs={'label':'precipitação (mm/dia'})
+#buffer.plot(ax=ax, transform=ccrs.PlateCarree(), alpha=0.5, facecolor='blue')
 
 shp_ma.plot(ax=ax, facecolor='none', linewidth=3) 
 shp_pa.plot(ax=ax, facecolor='none', linewidth=3)
 shp.plot(ax=ax)
 plt.axis('equal')
+
+
+
+
 plt.show()
 quit()
 
 
 
 
-#ax.set_xlabel('precipitação (mm/dia)')
-#area = box(min_longitude, min_latitude, max_longitude, max_latitude)
-#area_dataframe = gpd.GeoDataFrame({'geometry': [area]}, crs=shp_ma.crs)
-#area_dataframe.plot(ax=ax, color='red', alpha=0.5)  # Plotar retângulo com transparência
-#area_dataframe_cordex.plot(ax=ax, color='yellow', alpha=0.5)  # Plotar retângulo com transparência
-#ax.add_feature(cartopy.feature.BORDERS, linestyle='-', linewidth=1)
-#ax.set_extent([Min_Longitude, Max_Longitude, Min_Latitude, Max_Latitude], crs=ccrs.PlateCarree())
 
-#ax.coastlines()
 
-#plt.ylabel('Precipitacao (mm/dia)')
 
+
+
+
+
+
+#data['pr'].isel(time=0).plot(ax=ax, transform=ccrs.PlateCarree(), cbar_kwargs={'label':'precipitação (mm/dia'})
+#colorbar = fig.colorbar(pr, ax=ax, orientation='vertical')
+#colorbar.set_label('Precipitação (mm/dia)') 
